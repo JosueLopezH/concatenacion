ac6902e7f4424fc5ef28d8e6f9c991bb8d1bee53
JosueLopezH <josuelopez4864@gmail.com>
2024-05-23 18:37:31 -0600

    sopas

---
+++ src/main/java/com/practica_concatenacion/concatenacion/PracticaConcatenacionApplication.java
@@ -0,0 +1,13 @@
+package com.practica_concatenacion.concatenacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaConcatenacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaConcatenacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica_concatenacion/concatenacion/PracticaConcatenacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_concatenacion.concatenacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaConcatenacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
